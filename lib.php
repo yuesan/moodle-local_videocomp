d5a6aed5604fec1afe1fcd984cf31a27dbe290b1
Takayuki Fuwa <yae.yue@gmail.com>
2018-05-10 23:53:03 +0900

    動画ファイルの場合のみJSを読み込む

--- lib.php
+++ lib.php
@@ -3,10 +3,21 @@
 defined('MOODLE_INTERNAL') || die();
 
 function local_videocomp_extend_navigation(global_navigation $navigation) {
-    global $CFG, $PAGE;
+    global $PAGE;
 
     $cm = $PAGE->cm;
     if (!empty($cm) && $cm->modname === 'resource') {
-        $PAGE->requires->js_call_amd('local_videocomp/listener', 'initialise', [$cm->id]);
+        $context = context_module::instance($cm->id);
+
+        $fs = get_file_storage();
+        $files = $fs->get_area_files($context->id, 'mod_resource', 'content', 0, 'sortorder DESC, id ASC', false);
+        if (count($files) >= 1) {
+            $file = reset($files);
+            unset($files);
+
+            if(strpos($file->get_mimetype(), 'video') !== false){
+                $PAGE->requires->js_call_amd('local_videocomp/listener', 'initialise', [$cm->id]);
+            }
+        }
     }
 }
